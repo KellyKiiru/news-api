23a0285c08a7d72ddefa9b9448a62072bc9710f7
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-04 02:07:16 +0300

    initialize the main module; set the NEWS_API_KEY in the app/config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from distutils.debug import DEBUG
 
 class Config:
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?category={}&language=en&from=2022-04-16&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
     pass
