3af5285ac462bd12d4c1035abcc8f249439d25a8
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-04 14:26:54 +0300

    uncomment previous config code in app/config

--- app/config.py
+++ app/config.py
@@ -1,18 +1,18 @@
-### Configuration files 
-#import os
-#from distutils.debug import DEBUG
+## Configuration files 
+import os
+from distutils.debug import DEBUG
 
-#class Config:
-#    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?category={}&language=en&from=2022-04-16&apiKey={}'
-#    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+class Config:
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?category={}&language=en&from=2022-04-16&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
-#class ProdConfig(Config):
-#    pass
+class ProdConfig(Config):
+    pass
 
-#class DevConfig(Config):
-#    DEBUG = True
+class DevConfig(Config):
+    DEBUG = True
 
-#config_options = {
-#'development':DevConfig,
-#'production':ProdConfig
-#}
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
