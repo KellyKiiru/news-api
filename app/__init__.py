0c75825f102fb4e5e75a06e7655ae6a597416d77
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-04 14:49:40 +0300

    write code that will make requests to our api in request.py; define the initialization of news objects in models.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,9 @@ app  = Flask(__name__)
 
 #setting up configuration
 app.config.from_object(DevConfig)
+#configuration that store the news api key
+app.config.from_object('config.py')
+
 
 #importing error and view files from app
 from app import views,error 
