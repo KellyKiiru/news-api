6b671f9d2abd411d30f2d155a18c8289d5579fee
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-05 21:35:05 +0300

    import bootstrap and config_options; define create app function and initialize bootstrapand register it

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,29 @@
+'''
+import bootstrap and configurations
+'''
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-from app.main import views
-from ..config import DevConfig
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    
+    #initializing flask
+    app  = Flask(__name__)
+
+    #setting up configuration
+    app.config.from_object(config_options[config_name])
+    
+    # Initializing bootstrap
+    bootstrap.init_app(app) 
+    
+    # Register the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
 
+    from .request import make_request
+    make_request(app) 
 
-#initializing flask
-app  = Flask(__name__)
-
-#setting up configuration
-app.config.from_object(DevConfig)
-#configuration that store the news api key
-app.config.from_pyfile('config.py')
-
-
-#importing error and view files from app
-from app.main import error 
+    return app
