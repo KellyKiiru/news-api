f3450f89316710c00d4d4282c14d0061acbcc39a
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-05 21:12:01 +0300

    define fumction to display error page; add code to error.html; define main blueprint and import views and error files

---
+++ app/main/error.py
@@ -0,0 +1,10 @@
+'''
+File to render the 404 error page
+'''
+from flask import render_template
+from . import main
+
+@main.app_errorhandler(404)
+def four_Ow_four(error):
+    
+    return render_template('error.html'),404
