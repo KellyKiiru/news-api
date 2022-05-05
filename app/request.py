792b30dec9022488fc4b2568427a7477e02e378f
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-05 22:13:27 +0300

    fix bugs in request.py

--- app/request.py
+++ app/request.py
@@ -1,8 +1,11 @@
-from app import app
 import urllib.request,json
 from .models import *
-#getting api key
-api_key = app.config['NEWS_API_KEY']
+
+# api key
+api_key = None
+# news base url
+base_url = None
+
 
 def make_request(app):
     global api_key,base_url
